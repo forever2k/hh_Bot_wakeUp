c704a7220178a7611f3a3d3eb40f403d10ea0ab0
d <d>
2021-02-18 23:10:34 +0300

    1

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ def main():
     chrome_options = Options()
     chrome_options.add_argument('--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, '
                                 'like Gecko) Chrome/85.0.4183.83 Safari/537.36')
-    # chrome_options.add_argument('headless')
+    chrome_options.add_argument('headless')
     chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
     driver = webdriver.Chrome(options=chrome_options)
 
