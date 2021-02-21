37251bff39aa43c609ab9905a294707a18be1b21
d <d>
2021-02-21 20:49:35 +0300

    1

--- app.py
+++ app.py
@@ -45,9 +45,11 @@ def send_welcome(message):
 
     bot.send_message(message.from_user.id, driver.current_url)
 
+    bot.send_message(message.from_user.id, 'before cycle')
     for item in ob:
         bot.send_message(message.from_user.id, 'Its a cycle')
         bot.send_message(message.from_user.id, item.text)
+    bot.send_message(message.from_user.id, 'after cycle')
 
     bot.send_message(message.from_user.id, "2 RES command finished")
 
