9faa15d36c2780bcb37b0060ae631c93432bcddc
d <d>
2021-02-22 14:05:45 +0300

    1

--- app.py
+++ app.py
@@ -60,11 +60,11 @@ def wake_up():
     bot.send_message(-1001364950026, "Updated finished")
 
 
-schedule.every(3).minutes.do(wake_up)
-
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+# schedule.every(3).minutes.do(wake_up)
+#
+# while True:
+#     schedule.run_pending()
+#     time.sleep(1)
 
 
 @server.route('/' + TOKEN, methods=['POST'])
