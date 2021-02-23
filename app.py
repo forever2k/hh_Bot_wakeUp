be7efe6cec3759f3184c49632cf7aa65aa2bc03c
d <d>
2021-02-23 10:50:10 +0300

    1

--- app.py
+++ app.py
@@ -62,17 +62,20 @@ def send_girl(message):
     driver.get(URL2)
     bot.send_photo(227722043, driver.current_url)
 
-    ob = driver.find_elements_by_class_name("pcsrt-th-pics")
+    ob = driver.find_elements_by_class_name("pcsrt-th-image--link")
     ob[1].click()
     r = driver.current_url
     url = r.url
 
     bot.send_photo(227722043, 'here')
     bot.send_photo(227722043, driver.current_url)
+    bot.send_photo(227722043, 'here 1')
     bot.send_photo(227722043, photo=url)
-    bot.send_photo(227722043, photo=r)
-    bot.send_photo(227722043, photo=ob[1])
     bot.send_photo(227722043, 'here 2')
+    bot.send_photo(227722043, photo=r)
+    bot.send_photo(227722043, 'here 3')
+    bot.send_photo(227722043, photo=ob[1])
+    bot.send_photo(227722043, 'here all')
 
 
 
