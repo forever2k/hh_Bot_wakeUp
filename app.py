2408bdb9e15f794c431b3255fa1f4bd677479abd
forever2k <forever2k@yandex.ru>
2021-03-21 18:54:18 +0300

    1

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ import time
 import random
 import requests
 
+
 bot = telebot.TeleBot(TOKEN)
 
 server = Flask(__name__)
@@ -41,106 +42,35 @@ launch = True
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     bot.send_message(message.from_user.id, "Bot hh_wakeUp works")
-    bot.send_message(message.from_user.id, message.from_user.id)
+    # bot.send_message(message.from_user.id, message.from_user.id)
 
 
 @bot.message_handler(commands=['res'])
-def send_welcome(message):
+def res(message):
 
-    bot.send_message(message.from_user.id, "RES command starts")
+    bot.send_message(message.from_user.id, "RES Bot starts to work")
+
+    start_res()
 
     schedule.every(250).minutes.do(wake_up)
-
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
-
-    bot.send_message(message.from_user.id, "Its END of RES")
-
-
-@bot.message_handler(commands=['stop'])
-def stop_send_girl(message):
-
-    stop_girl()
-    bot.send_message(message.from_user.id, "Send girl Bot finished work")
-
-
-def stop_girl():
-
-    global launch
-    launch = False
-
-def start_girl():
-
-    global launch
-    launch = True
-
-
-@bot.message_handler(commands=['send'])
-def send_girl(message):
-
-    bot.send_message(message.from_user.id, "Send Bot works")
-
-    start_girl()
-    bot.send_message(message.from_user.id, "Start Bot is activated")
-    girl()
-    bot.send_message(message.from_user.id, "First girl completed")
-
-
-    schedule.every(2).minutes.do(girl)
 
     while launch:
         schedule.run_pending()
         time.sleep(1)
 
 
-def girl():
+@bot.message_handler(commands=['stop'])
+def stop_res(message):
 
-    page = random.randrange(1, 10)
-    URL2 = 'https://xxx.pics/category/cute/' + str(page) + '/'
-    guys = ['парни', 'ребятушки', 'братушки', 'ребятки', 'мужики', 'перцы', 'эксперты', 'экспертное сообщество', 'мои герои', 'сладкие мои', 'chicos', 'sexo masculino']
-    greeting = ['здарова', 'хая', 'салам', 'салют', 'здравствуйте', 'шалом', 'бонжур', 'хэллоу', 'хей', 'буэнос диас',
-                'хола', 'доброго дня', 'добрый день', 'ассалам алейкум', 'hola', 'prosperadlo', 'hola mis queridos']
-    phrases = ['как вам мои чики?', 'попробуйте меня', 'какая я вкусненькая', 'смотрите на мои вишенки',
-               'как вам мои изюминки?', 'я вся горю', 'початимся?', 'пообщаемся?',
-               'ох, не смотри на меня так', 'мои булочки готовы для вас', 'рада тут побывать',
-               'всегда готова, жду вас тут', 'порадуйте меня чем нибудь', 'я секси, да?', 'я конфетка, да?',
-               'сейчас позову подружек не хуже меня', 'сегодня здесь будет жарко', 'я вся горю',
-               'классный денек сегодня, да?', 'погодка не фонтан, согрейте меня', 'всем хорошего дня!',
-               'всем классного дня!', 'заходите поглядеть на меня еще', 'хватит палитьтся на мои титьки', 'как я вам?', 'оцените меня экспертно', 'не сломайте об меня глаза', 'сиськи заказывали?', 'как вам мои шары?']
-    emoji = ['$)', ':)', ';)', 'oO', ':**', ' ', '..', 'уух', 'мм;)']
+    global launch
+    launch = False
+    bot.send_message(message.from_user.id, "STOP is activated")
 
-    guys_random = random.randrange(0, len(guys))
-    greeting_random = random.randrange(0, len(greeting))
-    phrases_random = random.randrange(0, len(phrases))
-    emoji_random = random.randrange(0, len(emoji))
 
-    willing_phrase = f'{guys[guys_random].capitalize()} {greeting[greeting_random]}! {phrases[phrases_random].capitalize()} {emoji[emoji_random]}'
+def start_res():
 
-    driver.get(URL2)
-    wait1 = WebDriverWait(driver, 10)
-
-    try:
-        path_to_pict = wait1.until(expected_conditions.visibility_of_all_elements_located((By.CLASS_NAME, 'pcsrt-th-lightgallery-item')))
-    except Exception as error:
-        bot.send_message(227722043, 'Item 1 not found')
-        bot.send_message(227722043, error)
-
-    try:
-        all_pict = len(path_to_pict)
-        pict_random = random.randrange(0, all_pict)
-        time.sleep(2)
-        pict = path_to_pict[pict_random].get_attribute('data-src')
-    except Exception as error:
-        bot.send_message(227722043, 'Item 2 not found')
-        bot.send_message(227722043, error)
-
-    try:
-        bot.send_photo(227722043, photo=pict)
-        bot.send_message(227722043, willing_phrase)
-    except Exception as error:
-        bot.send_message(227722043, 'Final error')
-        bot.send_message(227722043, error)
+    global launch
+    launch = True
 
 
 def wake_up():
@@ -148,10 +78,17 @@ def wake_up():
     bot.send_message(227722043, "Function Wake_up starts")
     driver.get(URL)
 
-    cookies = pickle.load(open("session", "rb"))
-    for cookie in cookies:
-        driver.add_cookie(cookie)
+    for cook in hh:
+        driver.add_cookie(cook)
+
     driver.refresh()
+    time.sleep(1)
+
+
+    # cookies = pickle.load(open("session", "rb"))
+    # for cookie in cookies:
+    #     driver.add_cookie(cookie)
+    # driver.refresh()
 
     ob = driver.find_elements_by_class_name("HH-Supernova-NaviLevel2-Link")
     ob[0].click()
@@ -170,12 +107,6 @@ def wake_up():
 
     bot.send_message(227722043, "Function Wake_up finished")
 
-
-# schedule.every(3).minutes.do(wake_up)
-
-# while True:
-#     schedule.run_pending()
-#     time.sleep(1)
 
 
 @server.route('/' + TOKEN, methods=['POST'])
--- config.py
+++ config.py
@@ -1,4 +1,6 @@
+import os
 
 TOKEN = '1619381009:AAEAlXyTON2xG6H-m2OaCAq96i63bdZyRpA'
 APP_NAME = 'https://hh-bot-wakeup.herokuapp.com/'
+hh = os.getenv('hh')
 
